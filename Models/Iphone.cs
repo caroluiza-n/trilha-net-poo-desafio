0fdd8258577aeefc9c848e96472cf28d74eccd77
caroluiza-n <caroluizanunes126@gmail.com>
2024-11-27 17:03:16 -0300

    Classes concluídas

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,12 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){
+            nomeApp = "Whatsapp";
+            Console.WriteLine($"Instalando o app {nomeApp}");
+        }
     }
 }
